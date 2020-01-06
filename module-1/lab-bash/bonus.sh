e64c329318aad68a62c8f9300b46f49b9acac296
Roberto Ross Leon <dev@rross.me>
2020-01-06 17:52:19 -0600

    Bonus completado

---
+++ module-1/lab-bash/bonus.sh
@@ -0,0 +1,20 @@
+#read name
+#echo $name
+#mkdir $name
+#rmdir $name
+
+for file in ./lorem/*
+do
+  echo $(basename "${file}") | sed -r "s/.*\///"
+done
+
+for file in ./lorem/*
+do
+  echo $file | sed -r "s/.+\/(.+)\..+/\1/"
+done
+
+for file in ./lorem/*
+do
+  #printf $(basename "${file}") | cut -d "." -f1 | wc -m # Is not working
+  printf ${file} | sed -r "s/.+\/(.+)\..+/\1/" | wc -m
+done
