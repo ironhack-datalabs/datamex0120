b54c7f163378b5cd59ec76958613f08763dbde13
hatchdavid1 <email>
2020-01-06 15:56:25 -0600

    'nuevo'

---
+++ module-1/lab-resolving-git-conflicts/git-fu.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 11:56:01 2020
+
+@author: Data Analyst
+"""
+
+def sumar(n = 333545):
+    n = n +3 
+    return n 
+
+if sumar < 10 :
+    sumar
+    print(sumar)
+    
