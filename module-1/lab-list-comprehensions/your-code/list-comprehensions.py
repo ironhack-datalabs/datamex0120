c6ef09b39fcb5c36e4cee755db1f996c33479a73
jibo16 <jorgevd2010@hotmail.com>
2020-01-07 18:02:30 -0600

    list-comprehension1

---
+++ module-1/lab-list-comprehensions/your-code/list-comprehensions.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+# 1. Use a list comprehension to create and print a list of consecutive integers starting with 1 and ending with 50
+
+list_compr = [i for i in range(1,50)]
+
+print(list_compr)
+
+# 2. Use a list comprehension to create and print a list of even numbers starting with 2 and ending with 200.
+
+even_num = [i for i in range(2,201) if i != 0 print(i)]
+
+print(even_num)
