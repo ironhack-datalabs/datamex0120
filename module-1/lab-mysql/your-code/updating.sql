e1e71595d4d10c4248487a5882be880f0148fa43
Roberto Ross Leon <dev@rross.me>
2020-01-13 16:41:14 -0600

    Bonus Updating done.

---
+++ module-1/lab-mysql/your-code/updating.sql
@@ -0,0 +1,8 @@
+/* FIXING MIAMI - USA */
+UPDATE Stores SET name="Suc. Miami", country="USA" WHERE id=5;
+
+
+/* ADDING EMAILS */
+UPDATE Customers SET email="ppicasso@gmail.com" WHERE id=1;
+UPDATE Customers SET email="lincoln@us.gov" WHERE id=2;
+UPDATE Customers SET email="hello@napoleon.me" WHERE id=3;
