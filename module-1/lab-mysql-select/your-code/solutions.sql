430d7e7d1a441dddb8c2f88af913988309191401
Roberto Ross Leon <dev@rross.me>
2020-01-14 13:56:56 -0600

    Challenge 2 done.

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,4 +1,5 @@
 /* Challenge 1 */
+/*
 SELECT  authors.au_id AS AUTHOR_ID,
         authors.au_lname AS LAST_NAME,
         authors.au_fname AS FIRST_NAME,
@@ -11,3 +12,21 @@ FROM titleauthor
   ON titleauthor.title_id = titles.title_id
   JOIN publishers
   ON titles.pub_id = publishers.pub_id;
+*/
+
+/* Challenge 2 */
+SELECT AUTHOR_ID, LAST_NAME, FIRST_NAME, PUBLISHER, COUNT(*) AS TITLE_COUNT
+FROM (
+	SELECT  authors.au_id AS AUTHOR_ID,
+			authors.au_lname AS LAST_NAME,
+			authors.au_fname AS FIRST_NAME,
+			title AS TITLE,
+			pub_name AS PUBLISHER
+	FROM titleauthor
+	  JOIN authors
+	  ON titleauthor.au_id = authors.au_id
+	  JOIN titles
+	  ON titleauthor.title_id = titles.title_id
+	  JOIN publishers
+	  ON titles.pub_id = publishers.pub_id) summary
+GROUP BY AUTHOR_ID, PUBLISHER;
