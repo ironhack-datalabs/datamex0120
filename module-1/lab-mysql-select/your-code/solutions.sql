de782afb703197101f0a7190c7816600d6be088d
Roberto Ross Leon <dev@rross.me>
2020-01-14 16:49:10 -0600

    Challenge 3 fixed, now sales.qty are considered instead of titles.

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -35,7 +35,7 @@ GROUP BY AUTHOR_ID, PUBLISHER;
 SELECT	authors.au_id AS AUTHOR_ID,
 		au_lname AS LAST_NAME,
         au_fname AS FIRST_NAME,
-        COUNT(*) AS TOTAL 
+        SUM(sales.qty) AS TOTAL 
 FROM sales
 	JOIN titleauthor
 	ON sales.title_id = titleauthor.title_id
@@ -46,11 +46,27 @@ ORDER BY TOTAL DESC
 LIMIT 3;
 
 
-/* Challenge 4 */
+/* Challenge 4 *//*
 SELECT	authors.au_id AS AUTHOR_ID,
 		au_lname AS LAST_NAME,
         au_fname AS FIRST_NAME,
         IFNULL(COUNT(sales.title_id), 0) AS TOTAL
+FROM titleauthor
+	RIGHT JOIN authors
+	ON titleauthor.au_id = authors.au_id
+    LEFT JOIN sales
+    ON titleauthor.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC;
+
+/* Bonus */
+/*
+SELECT	authors.au_id AS AUTHOR_ID,
+		au_lname AS LAST_NAME,
+        au_fname AS FIRST_NAME,
+        IFNULL(COUNT(sales.title_id), 0) AS TOTAL
+        /*AS PROFIT*/
+/*
 FROM titleauthor
 	RIGHT JOIN authors
 	ON titleauthor.au_id = authors.au_id
