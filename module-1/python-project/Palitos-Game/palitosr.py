6279b2b9cff8eeacc64359487f3734704d0da3b5
juliocrn94 <juliocrn94@gmail.com>
2020-01-14 10:11:18 -0600

    functional v1.2

--- module-1/python-project/Palitos-Game/palitosr.py
+++ module-1/python-project/Palitos-Game/palitosr.py
@@ -1,9 +1,10 @@
 from palitosb import *
 import random
-#from os import system
+from os import system
 
 nom1 = input("Enter Player1's name: ")
 nom2 = input("Enter Player2's name: ")
+system('clear')
 
 jug1 = manos(nom1)
 jug2 = manos(nom2)
@@ -12,13 +13,18 @@ lista_jug = [jug1,jug2]
 turn = random.randint(1,2)
 
 print('\n'+score(jug1, jug2))
-
+t_c=1
 while True:
     print("\nTurno de {}!".format(lista_jug[turn%2].nombre))
-
-    mano_at = int(input("Mano de ataque\n('0' para I,'1' para D)? "))
-    mano_def = int(input("Mano de oponente\n('0' para I,'1' para D)? "))
-
+    while True:
+        mano_at = int(input("Mano de ataque\n('0' para I,'1' para D)? "))
+        mano_def = int(input("Mano de oponente\n('0' para I,'1' para D)? "))
+        if (mano_at in [0,1]) and (mano_def in [0,1]):
+            break
+        else:
+            print("Porfavor eligue entre '0' ó '1'!")
+    system('clear')
+    print('Turno: {}'.format(t_c))
     print(lista_jug[turn%2].ataque(lista_jug[(turn+1)%2], mano_at, mano_def))
     print(score(jug1, jug2))
     
@@ -27,4 +33,5 @@ while True:
         print("\nGAME OVER\nWinner is {}".format(winner))
         break
     turn+=1
+    t_c+=1
     
