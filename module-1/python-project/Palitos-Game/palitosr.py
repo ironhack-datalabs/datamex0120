6a212d55f40fcde2f2a7577dd48edc208d2a8867
juliocrn94 <juliocrn94@gmail.com>
2020-01-14 09:28:13 -0600

    Se agrega juego palitos v1

---
+++ module-1/python-project/Palitos-Game/palitosr.py
@@ -0,0 +1,30 @@
+from palitosb import *
+import random
+#from os import system
+
+nom1 = input("Enter Player1's name: ")
+nom2 = input("Enter Player2's name: ")
+
+jug1 = manos(nom1)
+jug2 = manos(nom2)
+
+lista_jug = [jug1,jug2]
+turn = random.randint(1,2)
+
+print('\n'+score(jug1, jug2))
+
+while True:
+    print("\nTurno de {}!".format(lista_jug[turn%2].nombre))
+
+    mano_at = int(input("Mano de ataque\n('0' para I,'1' para D)? "))
+    mano_def = int(input("Mano de oponente\n('0' para I,'1' para D)? "))
+
+    print(lista_jug[turn%2].ataque(lista_jug[(turn+1)%2], mano_at, mano_def))
+    print(score(jug1, jug2))
+    
+    if check_win(jug1, jug2):
+        winner = check_who_win(jug1, jug2)
+        print("\nGAME OVER\nWinner is {}".format(winner))
+        break
+    turn+=1
+    
