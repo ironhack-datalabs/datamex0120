4d62a2854e8421df95f8d7dd7e89cc58fe329905
jibo16 <jorgevd2010@hotmail.com>
2020-01-15 08:39:49 -0600

    advanced mysql finalizado

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,3 +1,5 @@
+
+# CHALLENGE 1
 SELECT authors.au_id AS AUTHOR_ID, 
 titleauthor.au_id AS TITLEAU_AUTHOR_ID, 
 titleauthor.title_id AS TITLEAU_TITLE_ID,
@@ -17,3 +19,36 @@ GROUP BY authors.au_id, titleauthor.au_id, titleauthor.title_id, titles.title_id
 ORDER BY sales_royalty DESC
 LIMIT 3;
 
+# CHALLENGE 2
+
+SELECT SUM(authors.au_id) AS AUTHOR_ID, 
+titleauthor.au_id AS TITLEAU_AUTHOR_ID, 
+titleauthor.title_id AS TITLEAU_TITLE_ID,
+titleauthor.royaltyper AS TITLEAU_ROYATYP,
+SUM(titles.title_id) AS TITLES_TITLE_ID,
+titles.price AS TITLES_PRICE,
+titles.royalty AS TITLES_ROYALTY,
+ROUND(titles.price * IFNULL(sales.qty,0) * titles.royalty / 100 * titleauthor.royaltyper / 100,4) AS sales_royalty
+FROM authors 
+RIGHT JOIN titleauthor 
+ON titleauthor.au_id = authors.au_id
+RIGHT JOIN titles
+ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales
+ON sales.title_id = titleauthor.title_id
+GROUP BY authors.au_id, titleauthor.au_id, titleauthor.title_id, titles.title_id, sales.qty
+ORDER BY sales_royalty DESC
+LIMIT 3;
+
+# CHALLENGE 3
+SELECT authors.au_id, ROUND(titles.price * IFNULL(sales.qty,0) * titles.royalty / 100 * titleauthor.royaltyper / 100,4) AS most_profiting_authors
+FROM authors 
+RIGHT JOIN titleauthor 
+ON titleauthor.au_id = authors.au_id
+RIGHT JOIN titles
+ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales
+ON sales.title_id = titleauthor.title_id
+GROUP BY authors.au_id, titles.price, sales.qty, titles.royalty, titleauthor.royaltyper
+ORDER BY most_profiting_authors DESC
+LIMIT 3;
