bd11b59a3b97b04355e1f46adb5ebb1166ef77eb
antareszul <antareszul@gmail.com>
2020-01-15 16:31:40 -0600

    final

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,6 +1,10 @@
+
+!/usr/bin/env python
+-*- coding: utf-8 -*-
+
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
@@ -8,57 +12,61 @@
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+a = np.random.random((2,3,5))
 
 
 
 #4. Print a.
-
+a
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
-
+b
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+a.size == b.size
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+i=a+b
+because not are the same shape
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.transpose(b, (1, 2, 0))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
-
+d=a+c
+because are the same shape 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+a
+d
 
 #12. Multiply a and c. Assign the result to e.
-
+e=a*c
 
 
 #13. Does e equal to a? Why or why not?
 
+e == a
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
 
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
