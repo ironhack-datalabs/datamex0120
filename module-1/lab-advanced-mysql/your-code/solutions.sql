5db57fbe0760b63f2bb0ddd5b8ba7596d9858611
albertoid <alberto.ibarra@gmail.com>
2020-01-15 13:56:30 -0600

    Done

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -23,3 +23,39 @@ GROUP BY au_id
 ORDER BY sum(sales_royalty) DESC
 LIMIT 3
 );
+
+/*---- CHALLENGE 2 ----*/
+
+SELECT au_id,sum(sales_royalty) 
+FROM
+	(SELECT ta.title_id, ta.au_id,t.price,t.royalty, (t.price * s.qty*t.royalty/100*ta.royaltyper/100) as sales_royalty
+	FROM 
+		titleauthor AS ta
+	INNER JOIN 
+		titles AS t ON t.title_id = ta.title_id
+	INNER JOIN sales AS s ON t.title_id = s.title_id)
+AS sales_royalty_table
+GROUP BY au_id
+ORDER BY sum(sales_royalty) DESC
+LIMIT 3;
+
+CREATE TABLE publications.most_profiting_authors (
+au_id VARCHAR(11) NOT NULL,
+profits FLOAT NULL,
+PRIMARY KEY(au_id));
+
+/*---- CHALLENGE 3 ----*/
+
+INSERT INTO most_profiting_authors 
+SELECT au_id,sum(sales_royalty) 
+FROM
+	(SELECT ta.title_id, ta.au_id,t.price,t.royalty, (t.price * s.qty*t.royalty/100*ta.royaltyper/100) as sales_royalty
+	FROM 
+		titleauthor AS ta
+	INNER JOIN 
+		titles AS t ON t.title_id = ta.title_id
+	INNER JOIN sales AS s ON t.title_id = s.title_id)
+AS sales_royalty_table
+GROUP BY au_id
+ORDER BY sum(sales_royalty) DESC
+LIMIT 3;
