1dc10e428ec4f159fd46909ad80821f60c193e98
Roberto Ross Leon <dev@rross.me>
2020-01-14 19:41:36 -0600

    Challenge 2 done... I think.

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -2,9 +2,6 @@
 /*Step 1 */
 SELECT	titleauthor.title_id AS TITLE_ID,
 		titleauthor.au_id AS AUTHOR_ID,
-        titleauthor.royaltyper,
-        titles.price, titles.royalty,
-        sales.qty,
         titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS SALES_ROYALTY
 FROM	titleauthor
 JOIN	titles ON titleauthor.title_id = titles.title_id
@@ -42,3 +39,28 @@ FROM	(
 			GROUP BY TITLE_ID, AUTHOR_ID
 		) authors_profit;
 
+/***** Challange 2 *****/
+DROP TABLE IF EXISTS Royalties;
+CREATE TEMPORARY TABLE Royalties
+SELECT	titleauthor.title_id AS TITLE_ID,
+		titleauthor.au_id AS AUTHOR_ID,
+        titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS SALES_ROYALTY,
+        titles.advance AS ADVANCE,
+		titleauthor.royaltyper AS ROYALTYPER
+FROM	titleauthor
+JOIN	titles ON titleauthor.title_id = titles.title_id
+JOIN	sales ON titleauthor.title_id = sales.title_id
+ORDER BY TITLE_ID;
+
+SELECT * FROM Royalties;
+
+DROP TABLE IF EXISTS AuthorsProfit;
+CREATE TEMPORARY TABLE AuthorsProfit
+SELECT TITLE_ID, AUTHOR_ID, SUM(SALES_ROYALTY) AS TOTAL_ROYALTY, ADVANCE, ROYALTYPER
+FROM Royalties
+GROUP BY TITLE_ID, AUTHOR_ID, ADVANCE, ROYALTYPER;
+
+SELECT * FROM AuthorsProfit;
+
+SELECT 	AUTHOR_ID, TOTAL_ROYALTY + ADVANCE * ROYALTYPER / 100 AS PROFITS
+FROM AuthorsProfit;
